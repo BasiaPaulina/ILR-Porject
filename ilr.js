0dca7b1de5acc43a2b5c886ce56defa9a603defc
BasiaPaulina <48137639+BasiaPaulina@users.noreply.github.com>
2019-06-11 18:33:27 +0100

    Update ilr.js

--- ilr.js
+++ ilr.js
@@ -1,4 +1,8 @@
-function password() {
-  prompt("Password:");
+let password = "ilrprojectinprogress";
+let result = prompt("Password:");
+
+if (result === password) {
+} else {
+  alert("Sorry this is a work in progress");
+  window.location = "https://www.google.com/";
 }
-password();
